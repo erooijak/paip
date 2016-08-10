b25a7cdd230b20513385ba25a31cf06aa4ddbb1d
Erwin Rooijakkers <ErwinRooijakkers@gmail.com>
2016-08-10 02:01:12 +0200

    Update to tests and exercise 1.5

---
+++ src/paip/exercise1_5.clj
@@ -0,0 +1,14 @@
+(ns paip.exercise1_5
+  (:gen-class))
+
+;; Exercise 1.5 [m] Write a function to compute the dot product of two sequences of numbers,
+;; represented as lists. The dot product is computed by multiplying corresponding elements
+;; and then adding up the resulting products.
+
+(defn dot-product
+  "Calculates the dot product. Assuming vectors are of even length and consist of numbers."
+  [coll1 coll2]
+  (if (empty? coll1) 0
+    (+ (* (first coll1) (first coll2))
+       (dot-product (rest coll1) (rest coll2)))))
+
